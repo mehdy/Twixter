c9c8976b6cb50c3c871feb533d5fcd3d9343a379
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2021-10-18 22:02:17 +0330

    Add Twitter api stuff
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,21 @@ module github.com/mehdy/twixter
 
 go 1.17
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/dghubble/go-twitter v0.0.0-20211002212826-ad02880e616b
+	github.com/google/uuid v1.3.0
+)
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
+)
--- twitter.go
+++ twitter.go
@@ -1,9 +1,20 @@
 package twixter
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
 	"time"
 
+	"github.com/dghubble/go-twitter/twitter"
 	"github.com/google/uuid"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+const (
+	// nolint: gosec // no credentials
+	twitterTokenURL = "https://api.twitter.com/oauth2/token"
 )
 
 // TwitterProfile represents a user's profile on Twitter.
@@ -35,4 +46,72 @@ type TwitterProfile struct {
 	JoinedAt            time.Time
 	FollowingsIDs       []string // TwitterID references
 	FollowersIDs        []string // TwitterID references
+}
+
+type Twitter struct {
+	api *twitter.Client
+}
+
+func NewTwitter() *Twitter {
+	creds := &clientcredentials.Config{
+		ClientID:     os.Getenv("TWITTER_CONSUMER_KEY"),
+		ClientSecret: os.Getenv("TWITTER_CONSUMER_SECRET"),
+		TokenURL:     twitterTokenURL,
+	}
+
+	return &Twitter{
+		api: twitter.NewClient(creds.Client(context.TODO())),
+	}
+}
+
+func (t *Twitter) GetProfile(username string) (*TwitterProfile, error) {
+	user, resp, err := t.api.Users.Show(&twitter.UserShowParams{ScreenName: username})
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch profile from twitter API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return t.toTwitterProfile(*user), nil
+}
+
+func (t *Twitter) toTwitterProfile(user twitter.User) *TwitterProfile {
+	joinedAt, err := time.Parse(time.RubyDate, user.CreatedAt)
+	if err != nil {
+		fmt.Printf("failed to parse joinedAt(%v) time for %q: %s", user.CreatedAt, user.ScreenName, err)
+	}
+
+	jsonEntites, err := json.Marshal(user.Entities)
+	if err != nil {
+		fmt.Printf("failed to marshal user.Entities(%v) to JSON for %q: %s", user.Entities, user.ScreenName, err)
+	}
+
+	var ent map[string]interface{}
+
+	err = json.Unmarshal(jsonEntites, &ent)
+	if err != nil {
+		fmt.Printf("failed to unmarshall user.Entities(%v) from JSON for %q: %s", ent, user.ScreenName, err)
+	}
+
+	return &TwitterProfile{
+		TwitterID:           user.IDStr,
+		Name:                user.Name,
+		Username:            user.ScreenName,
+		Location:            user.Location,
+		Bio:                 user.Description,
+		URL:                 user.URL,
+		Email:               user.Email,
+		ProfileBannerURL:    user.ProfileBannerURL,
+		ProfileImageURL:     user.ProfileImageURLHttps,
+		Verified:            user.Verified,
+		Protected:           user.Protected,
+		DefaultProfile:      user.DefaultProfile,
+		DefaultProfileImage: user.DefaultProfileImage,
+		FollowersCount:      user.FollowersCount,
+		FollowingsCount:     user.FriendsCount,
+		FavouritesCount:     user.FavouritesCount,
+		ListedCount:         user.ListedCount,
+		TweetsCount:         user.StatusesCount,
+		Entities:            ent,
+		JoinedAt:            joinedAt,
+	}
 }
