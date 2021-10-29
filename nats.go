cda1685b18a9a6cfa64f5b27f40b23df99bd0e8b
Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>
2021-10-29 15:12:10 +0330

    Add Run method to nats server
    Signed-off-by: Mehdy Khoshnoody <mehdy.khoshnoody@gmail.com>

--- nats.go
+++ nats.go
@@ -1,13 +1,19 @@
 package twixter
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/nats-io/nats.go"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 type Nats struct {
-	*nats.EncodedConn
+	nats *nats.EncodedConn
+	log  *logrus.Logger
+	subs []*nats.Subscription
 }
 
 func NewNats(config *viper.Viper, log *logrus.Logger) *Nats {
@@ -21,5 +27,29 @@ func NewNats(config *viper.Viper, log *logrus.Logger) *Nats {
 		log.WithError(err).Fatal("Failed to connect to nats")
 	}
 
-	return &Nats{ec}
+	return &Nats{ec, log, make([]*nats.Subscription, 0)}
+}
+
+func (n *Nats) RegisterWorker(subject, queue string, handler nats.Handler) error {
+	sub, err := n.nats.QueueSubscribe(subject, queue, handler)
+	if err != nil {
+		n.log.WithError(err).Fatal("Failed to subscribe to nats")
+		return err
+	}
+	n.subs = append(n.subs, sub)
+
+	return nil
+}
+
+func (n *Nats) Run() {
+	quitChannel := make(chan os.Signal, 1)
+	signal.Notify(quitChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-quitChannel
+	n.log.Info("Shutting Down...")
+
+	for _, sub := range n.subs {
+		if err := sub.Unsubscribe(); err != nil {
+			n.log.WithError(err).Error("Failed to unsubscribe")
+		}
+	}
 }
